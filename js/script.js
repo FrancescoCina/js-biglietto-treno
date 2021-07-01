58ea8bb29efb5f57613614af44aed7c0ec93811a
FrancescoCina <francesco.cina04@gmail.com>
2021-07-01 15:48:07 +0200

    Collegato script js e richieste variabili km, età e prezzo

---
+++ js/script.js
@@ -0,0 +1,33 @@
+console.log("JS Ok");
+
+
+/* 
+Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
+Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+il prezzo del biglietto è definito in base ai km (0.21 € al km)
+va applicato uno sconto del 20% per i minorenni
+va applicato uno sconto del 40% per gli over 65.
+L'output del prezzo finale va stampato in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
+*/
+
+/* 
+PASSAGGI:
+
+1 - richiediamo numero di chilometri che l'utente deve percorrere (var = km + (parseint)prompt)
+2 - richiesdiamo l'età del passeggero (var = age + parseint(prompt))
+3 - definire il prezzo del biglietto seguendo le regole (var prezzo = alla variabile km * 0.21€/km)
+4 - applicare sconto per minorenni (if minorenne la variabile price *20% di sconto)
+5 - applicare sconto per over 65 - (if over65 la variabile price *40% di sconto)
+6 - stampare prezzo con massimo due decimali --> console.log del price - max due numeri decimali -
+*/
+
+
+var km = parseInt(prompt("Quanti chilometri devi percorrere?"));
+console.log("Chilometri da percorrere", km);
+
+var age = parseInt(prompt("Quanti anni hai?"));
+console.log("Anni del passeggero", age);
+
+var price = km * 0.21;
+console.log("Prezzo biglietto", price, " €");
+document.getElementById("price").innerHTML = price + " €";
